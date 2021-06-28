d1fa640643afbef4a185871cc62b30af479f12e3
dfuvx98 <dfx_98@hotmail.com>
2021-06-27 22:57:05 -0500

    añadir navbar para rol admin (falta optimizar)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,9 +52,19 @@
                                 </li>
                             @endif
                         @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('especialidades.index') }}">{{ __('Gestion especialidades') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('personaMostrarMedicos') }}">{{ __('Gestion medicos') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Gestion especialidades') }}</a>
+                        </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
+                                    {{ Auth::user()->role }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
